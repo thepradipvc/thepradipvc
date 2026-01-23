9768c8cd0844d427d404ddb6af32466ecc369fb9
Pradip Chaudhary <88336529+thepradipvc@users.noreply.github.com>
2026-01-23 13:38:33 +0530

    Static deploy config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
